6f3c32b995ab6db6fa77f82afb8bb8d0530bf1f1
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-11-28 17:45:27 +0100

    Add author

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<?php
+/**
+ * @author Raúl Caro Pastorino
+ * @copyright Copyright © 2017 Raúl Caro Pastorino
+ * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ */
+?>
