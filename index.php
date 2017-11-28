ad82fedcddc14ce71c996ab2ed3e63614c42d5c2
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-11-28 18:11:54 +0100

    Separada cabecera

--- index.php
+++ index.php
@@ -7,22 +7,12 @@
  */
 ?>
 <html lang="es">
-    <head>
-        <meta charset="utf-8">
-        <title>Librería</title>
-        <!-- Versión compilada y comprimida del CSS de Bootstrap -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+    <?php require 'head.php' ?>
 
-        <!-- Tema opcional -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous" />
-
-        <!-- Versión compilada y comprimida del JavaScript de Bootstrap -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    </head>
     <body>
         <?php require 'header.php' ?>
 
-        <div id="">
+        <div id="boxContent">
 
         </div>
 
