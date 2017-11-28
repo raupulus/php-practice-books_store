13134665aa30055af7f7b7d96eb4070a31fff139
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-11-28 18:59:32 +0100

    Estilos de footer añadidos

--- footer.php
+++ footer.php
@@ -6,13 +6,16 @@
  */
 ?>
 
-<footer>
-    copyright
-    <address>
-        Dirección
-    </address>
+<footer id="footer" class="footer">
+    <div class="container">
+        <p class="text-header">copyright</p>
 
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://getbootstrap.com/docs/3.3/dist/js/bootstrap.min.js"></script>
+        <address>
+            Dirección
+        </address>
+    </div>
 </footer>
+
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://getbootstrap.com/docs/3.3/dist/js/bootstrap.min.js"></script>
