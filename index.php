e0ae4dfdc2648aa413631bb48c575cbb08f61bf9
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-11-29 16:58:57 +0100

    Estilos y tabla principal agregado

--- index.php
+++ index.php
@@ -10,8 +10,24 @@
     <?php require 'head.php' ?>
     <body>
         <?php require 'header.php' ?>
-        <div id="boxContent" class="content">
+        <div id="boxContent" class="content container">
+            <div class="col-md-10 col-md-offset-1">
+                <table class="table table-striped table-bordered">
+                    <tr>
+                        <th>Nombre</th>
+                        <td>Autor</td>
+                        <td>Año</td>
+                        <td>Descripción</td>
+                    </tr>
 
+                    <tr>
+                        <td>El libro de la selva</td>
+                        <td>Pepe Cervatana</td>
+                        <td>1493</td>
+                        <td>Pepe camina por la selva y escribe un libro</td>
+                    </tr>
+                </table>
+            </div>
         </div>
         <?php require 'footer.php' ?>
     </body>
